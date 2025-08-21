55e8dac0e62b920b571d64f88e756bfb2eb40d12
Prajesh Singh Meena <prajeshmeena10b@gmail.com>
2025-08-21 13:19:13 +0530

    int commit

---
+++ src/MyMain.java
@@ -0,0 +1,7 @@
+public class MyMain {
+    public static void main(String[] args) {
+        College_Student cs = new College_Student("Piyush", 63);
+        cs.attend_class();
+        cs.give_exam();
+    }
+}
