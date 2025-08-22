e5aa11c663578c3a66d47d434bf2c11c2c7871ae
Prajesh Singh Meena <prajeshmeena10b@gmail.com>
2025-08-22 12:28:29 +0530

    int commit

---
+++ src/MyPerson.java
@@ -0,0 +1,10 @@
+public class MyPerson {
+    public static void main(String[] args) {
+//        Person p = new Person("Ghaziabad");
+//        p.getAddress();
+
+        SchoolStudent st = new SchoolStudent("Ghaziabad", "B. Tech");
+        st.getAddress();
+        st.getCourse();
+    }
+}
