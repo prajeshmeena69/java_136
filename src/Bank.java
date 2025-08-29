401969d950f88e707267ee55b8ab6d427a36443a
Prajesh Singh Meena <prajeshmeena10b@gmail.com>
2025-08-29 11:49:27 +0530

    Java 29 August Runtime Exception

---
+++ src/Bank.java
@@ -0,0 +1,13 @@
+public class Bank {
+    private int balance = 10000;
+    public void withdrawal(int amount) {
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Cannot withdraw amount greater than Balance.");
+        }
+
+        else {
+            balance = balance - amount;
+            System.out.println("Withdraw Successfully, Remaining Balance = " + balance);
+        }
+    }
+}
---
+++ src/MyBank.java
@@ -0,0 +1,6 @@
+public class MyBank {
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        b.withdrawal(1200);
+    }
+}
