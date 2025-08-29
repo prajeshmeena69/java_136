7aab054d8084c32af0803cb76c18d15ee8db36a2
Prajesh Singh Meena <prajeshmeena10b@gmail.com>
2025-08-29 11:26:30 +0530

    Java 29 August User Defined Exception

---
+++ src/MyNewException.java
@@ -0,0 +1,13 @@
+public class MyNewException {
+    public static void main(String[] args) {
+        UserDefineException udf = new UserDefineException();
+
+        try {
+            udf.validate(17);
+        }
+
+        catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
