455a84f91370845d8e9f9e2956d4cbb7d7c25a6b
Prajesh Singh Meena <prajeshmeena10b@gmail.com>
2025-09-02 12:31:52 +0530

    Java 2 September

---
+++ src/Unit_3/Array_List.java
@@ -0,0 +1,14 @@
+package Unit_3;
+
+import java.util.ArrayList;
+
+public class Array_List {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(11);
+        al.add(12);
+        al.add(13);
+        System.out.println(al);
+    }
+}
